54bd1bc53fcd9916b7657eb979423c7cf739532e
Breno Chiori <112735329+BrenoChiori@users.noreply.github.com>
2024-04-07 13:02:33 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+// let hello = "Hello World!"
+// alert(hello)
+
+// let n1 = 5
+// let n2 = 9
+// alert(n1+n2)
+
+// let variavel = 7
+// if(typeof variavel === "number") {
+//     alert("É um número")
+// } else (
+//     alert("Não é um número")
+// )
+
+// let variavel = "olá"
+// if(typeof variavel === "string") {
+//     alert("É uma string")
+// } else (
+//     alert("Não é uma string")
+// )
+
+// let variavel = false
+// if(typeof variavel === "boolean") {
+//     alert("É um booleano")
+// } else (
+//     alert("Não é um booleano")
+// )
+
+// let n1 = 7
+// let n2 = 1
+// alert(n1-n2)
+
+// let n1 = 12
+// let n2 = 7
+// alert(n1*n2)
+
+// let n1 = 16
+// let n2 = 4
+// alert(n1/n2)
+
+// let num = 16
+// if((num%2) == 0) {
+//     alert("É um número par")
+// } else {
+//     alert('Não é um número par')
+// }
+
+// let num = 17
+// if((num%2) != 0) {
+//     alert("É um número ímpar")
+// } else {
+//     alert('Não é um número ímpar')
+// }
